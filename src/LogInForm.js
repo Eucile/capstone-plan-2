def6f54810a9a100805e397414d1d1d82374d9e1
Krystal Foster <kmfoster777@gmail.com>
2018-12-09 12:57:27 -0800

    add glyph to login component

--- src/LogInForm.js
+++ src/LogInForm.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './assets/styles/LogInForm.css';
+import ee from './assets/images/eucileglyph.png';
 
 class LogInForm extends React.Component{
   render() {
     return(
       <div className="LogInStyles">
       <form className="user-sign-in">
+      <img className="e-styles" src={ee}/>
       <p className="p-sign">Sign into your account.</p>
           <input className="input-1"
             type='text'
