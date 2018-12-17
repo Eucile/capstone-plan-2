314863ea97156a90df1dfb7e97b42116c0599ba7
Krystal Foster <kmfoster777@gmail.com>
2018-12-17 11:15:44 -0800

    resolve firebase login bug

--- src/LogInForm.js
+++ src/LogInForm.js
@@ -6,7 +6,8 @@ import ee from './assets/images/eucileglyph.png';
 
 class LogInForm extends React.Component{
 
-  handleLogin = () => {
+  handleLogin = (event) => {
+    event.preventDefault();
     this.props.firebase.auth().signInWithEmailAndPassword(this.email.value, this.password.value).then(console.log('login')).catch(function(error) {
       alert('nope');
       var errorCode = error.code;
